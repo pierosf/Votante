dd3d360372a5bb9df9028612e506851f51c44b83
Piero Maritano <pieromaritano@gmail.com>
2023-12-06 08:48:50 -0300

    agrego base de datos

--- Program.cs
+++ Program.cs
@@ -1,31 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-List<Votante> _votantes = new();
 
-app.MapGet("/votantes", () => TypedResults.Ok(_votantes));
-app.MapGet("/votantes/{id}", ([FromRoute] int id) => TypedResults.Ok(_votantes.FirstOrDefault(v => v.Id == id)));
-app.MapPost("/votantes", ([FromBody] Votante _nuevoVotante) => {
-    _nuevoVotante.Id = _votantes.Count + 1;
-    _votantes.Add(_nuevoVotante);
+var connection = new SqliteConnection("DataSource=votos.db");
+connection.Open();
+builder.Services.AddDbContext<VotoDb>(opt => opt.UseSqlite(connection));
+
+
+var app = builder.Build();
+
+app.MapGet("/votantes", (VotoDb _db) => TypedResults.Ok(_db.Votantes.ToList()));
+app.MapGet("/votantes/{id}", (VotoDb _db, [FromRoute] int id) => TypedResults.Ok(_db.Votantes.Find(id)));
+app.MapPost("/votantes", (VotoDb _db, [FromBody] Votante _nuevoVotante) => {
+    _db.Votantes.Add(_nuevoVotante);
+    _db.SaveChanges();
     return TypedResults.Ok(_nuevoVotante.Id);
 });
-app.MapPut("/votantes/{id}", ([FromRoute] int id, [FromBody] Votante _votanteActualizado) =>  {
-    foreach (var _votante in _votantes)
-    {
-        if (_votante.Id == id) 
-        {
-            _votante.Nombre = _votanteActualizado.Nombre;
-            _votante.Apellido = _votanteActualizado.Apellido;
-            _votante.DNI = _votanteActualizado.DNI;
-            _votante.FechaNacimiento = _votanteActualizado.FechaNacimiento;
-        }
-    }
+app.MapPut("/votantes/{id}", (VotoDb _db, [FromRoute] int id, [FromBody] Votante _votanteActualizado) =>  {
+    var _votante = _db.Votantes.Find(id);
+    _votante.Nombre = _votanteActualizado.Nombre;
+    _votante.Apellido = _votanteActualizado.Apellido;
+    _votante.DNI = _votanteActualizado.DNI;
+    _votante.FechaNacimiento = _votanteActualizado.FechaNacimiento;
+    _db.SaveChanges();
     return TypedResults.Ok();
 });
-app.MapDelete("/votantes/{id}", ([FromRoute] int id) => {
-    _votantes = _votantes.Where(c => c.Id != id).ToList();
+app.MapDelete("/votantes/{id}", (VotoDb _db, [FromRoute] int id) => {
+    var _votante = _db.Votantes.Find(id);
+    _db.Votantes.Remove(_votante);
+    _db.SaveChanges();
     return TypedResults.Ok();
 });
 
@@ -38,4 +43,13 @@ public class Votante
     public required string Apellido { get; set; }
     public required string DNI {get; set;}
     public required string FechaNacimiento { get; set; }
+}
+
+public class VotoDb : DbContext
+{
+    public VotoDb(DbContextOptions<VotoDb> options) : base(options) { 
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Votante> Votantes => Set<Votante>();
 }
